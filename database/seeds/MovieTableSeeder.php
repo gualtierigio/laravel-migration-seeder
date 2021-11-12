a61166728586732bd8e56db7911dd4113a64fe38
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-11-12 17:48:52 +0100

    fake movies creati

--- database/seeds/MovieTableSeeder.php
+++ database/seeds/MovieTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Movie;
+use App\Model\Movie;
+use Faker\Generator as Faker;
 
 class MovieTableSeeder extends Seeder
 {
@@ -10,16 +11,28 @@ class MovieTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $movies = [];
+        // $movies = [];
 
-        foreach($movies as $movie) {
+        // foreach($movies as $movie) {
+        //     $newMovie = new Movie();
+        //     $newMovie->title = $movie['title'];
+        //     $newMovie->description = $movie['description'];
+        //     $newMovie->director = $movie['director'];
+        //     $newMovie->save();
+    
+
+        for ($i = 0 ; $i < 100; $i++) {
             $newMovie = new Movie();
-            $newMovie->title = $movie['title'];
-            $newMovie->description = $movie['description'];
-            $newMovie->director = $movie['director'];
+            $newMovie->title = $faker->words(3, true);
+            $newMovie->description = $faker->paragraph(8, true);
+            $newMovie->director = $faker->name();
+            
+            $newMovie->slug = Str::slug($newMovie->title, '-');
             $newMovie->save();
+
         }
+        
     }
 }
